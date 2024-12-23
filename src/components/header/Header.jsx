1fbddd86af51e448a6422d0c864e63cd90bfb619
bawuj <bawujdevchain@gmail.com>
2024-12-23 23:03:36 +0100

    new update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './header.css'
 import Nav from "../nav/Nav.jsx"
 
 const Header = () => {
 	const contractAddress = "DCn76VTnBHQ9TfBGVmVKQgECNiywbj7Pxbk125bjpump";
-	const exchangeUrl = "";
+	const exchangeUrl = "https://dexscreener.com/solana/5dmxfkmf943mjwegwtw5rb95ratijmvpmm6vqcxieh1f";
   return (
     <>
     <div className="grid-container header">
@@ -20,7 +21,7 @@ const Header = () => {
 </div>
 <div className = "banner-content">
 <div className="image__container">
-	<img src="../../../public/img/photo_2024-12-22_20-23-47.jpg" alt="" width="600px" height="400px"/>
+	<img src="/img/photo_2024-12-22_20-23-47.jpg" alt="" width="600px" height="400px"/>
 </div>
 <div className="text-container">
 	<div className="text-wrapper">
@@ -28,7 +29,7 @@ const Header = () => {
 		<p>Contract Addres: <span>{contractAddress}</span></p>
 	</div>
 	<div className="button__container">
-		<button className='btn'>Purchase $LIFE</button>
+		<button className='btn'><Link to={exchangeUrl}></Link>Purchase $LIFE</button>
 	</div>
 </div>
 </div>
